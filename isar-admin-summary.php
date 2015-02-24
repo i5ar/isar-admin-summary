16164610e9f14af7e5fe2d2e170cfd168db9e7b9
i5ar <i5ar@live.it>
2015-02-24 14:04:34 +0100

    Update .gitignore

--- isar-admin-summary.php
+++ isar-admin-summary.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Plugin Name: iSar Admin Summary
- * Plugin URI: https://github.com/i5ar/isar-admin-summary/
+ * Plugin URI: https://github.com/i5ar/isar-admin-summary
  * Description: The iSar Admin Summary also known as iSummary shows the latest posts from one or more sites of your interest in the iSummary pages of your admin panel. You can manage feeds to show within images or not, the number of posts to show and even a supplementary menu in your admin menu bar.
  * Version: 1.0.4
  * Author: Pierpaolo Rasicci
- * Author URI: http://isarch.it/three.html
+ * Author URI: http://three.isarch.it
  * Text Domain: isar-admin-summary
  * Domain Path: /languages/
  * License: GPL
